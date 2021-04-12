2a7139dc3d3de751e7443f49f33db7ebe393366e
abhisek <abhisekcsce@gmail.com>
2021-04-12 19:20:37 +0530

    Setting up viewBinding and menu options for fragments

--- app/src/main/java/com/example/todo/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/todo/fragments/add/AddFragment.kt
@@ -1,19 +1,38 @@
 package com.example.todo.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.todo.R
+import com.example.todo.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+    private var _binding:FragmentAddBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        // Inflating the layout for this fragment with viewBinding
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        //set options menu in Add Fragment
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    //overriding for viewBinding
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    //overriding for setHasOptionsMenu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu,menu)
     }
 }
--- app/src/main/java/com/example/todo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todo/fragments/list/ListFragment.kt
@@ -1,19 +1,49 @@
 package com.example.todo.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.todo.R
+import com.example.todo.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    //viewBinding variables
+    private var _binding:FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        // Inflating the layout for this fragment using viewBinding
+        _binding = FragmentListBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        //Floating action button to Navigate from list to add fragment
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        //Floating action button to Navigate from list to update fragment
+        binding.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        //Set menu option on ListFragment
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    //Overriding for viewBinding
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    //Overriding for setHasOptionsMenu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu,menu)
     }
 }
