2f6ae24426dcfb861635a3ea11707541d029044e
abhisek <abhisekcsce@gmail.com>
2021-04-12 19:19:21 +0530

    Setting up Action bar

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //setting up action bar with navigation controller
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+    //overriding onSupportNavigateUp for action bar(setUpActionBarWithNavController)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
