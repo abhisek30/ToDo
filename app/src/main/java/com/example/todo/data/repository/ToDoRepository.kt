aa3d71c2199e2afbe89ee8249dd6dea62a8c3ed1
abhisek <abhisekcsce@gmail.com>
2021-04-16 00:22:29 +0530

    Adding repository to project

---
+++ app/src/main/java/com/example/todo/data/repository/ToDoRepository.kt
@@ -0,0 +1,32 @@
+package com.example.todo.data.repository
+
+import androidx.lifecycle.LiveData
+import com.example.todo.data.ToDoDao
+import com.example.todo.data.models.ToDoData
+
+//to get reference of ToDodao , add parameter to it
+class ToDoRepository(private val toDoDao : ToDoDao) {
+
+    //using toDodao to get access from getAllData fun from ToDoDao class and hold reference from dao to repository
+    val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
+
+    //to insert data in DB pass ToDoData to this fun and it will pass on to toDoDoa insertData fun with todoData as parameter
+    suspend fun insertData(toDoData: ToDoData){
+        toDoDao.insertData(toDoData)
+    }
+
+    //to update data in DB pass ToDoData to this fun and it will pass on to toDoDoa updateData fun with todoData as parameter
+    suspend fun updateData(toDoData: ToDoData){
+        toDoDao.updateData(toDoData)
+    }
+
+    //to delete data in DB pass ToDoData to this fun and it will pass on to toDoDoa deleteData fun with todoData as parameter
+    suspend fun deleteItem(toDoData: ToDoData){
+        toDoDao.deleteItem(toDoData)
+    }
+
+    //to delete all the data in DB call toDoDoa deleteAll fun
+    suspend fun deleteAll(){
+        toDoDao.deleteAll()
+    }
+}
