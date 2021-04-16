a4126af9a6ad6fdbe71c90db0881d19a90ba4b2a
abhisek <abhisekcsce@gmail.com>
2021-04-17 01:08:29 +0530

    Adding BindingAdapters to apply data binding

---
+++ app/src/main/java/com/example/todo/fragments/BindingAdapters.kt
@@ -0,0 +1,89 @@
+package com.example.todo.fragments
+
+import android.os.Build
+import android.view.View
+import android.widget.Spinner
+import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.findNavController
+import com.example.todo.R
+import com.example.todo.data.models.Priority
+import com.example.todo.data.models.ToDoData
+import com.example.todo.fragments.list.ListFragmentDirections
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class BindingAdapters {
+
+    companion object {
+
+        //Binding adapter to navigate from list to add fragment
+        //used inside floating action button in fragment_list layout
+        @BindingAdapter("android:navigateToAddFragment")
+        @JvmStatic
+        fun navigateToAddFragment(view: FloatingActionButton, navigate: Boolean) {
+            view.setOnClickListener {
+                if (navigate) {
+                    view.findNavController().navigate(R.id.action_listFragment_to_addFragment)
+                }
+            }
+        }
+
+        //Binding adapter to display imageView and textView if DB has data or not
+        //used inside imageView and textView (fragment_list layout)
+        @BindingAdapter("android:emptyDatabase")
+        @JvmStatic
+        fun emptyDatabase(view: View, emptyDatabase: MutableLiveData<Boolean>) {
+            when (emptyDatabase.value) {
+                true -> view.visibility = View.VISIBLE
+                false -> view.visibility = View.INVISIBLE
+            }
+        }
+
+        //Binding adapter to display current priority in update Fragment
+        @BindingAdapter("android:parsePriorityToInt")
+        @JvmStatic
+        fun parsePriorityToInt(view: Spinner, priority: Priority) {
+            when (priority) {
+                Priority.HIGH -> {
+                    view.setSelection(0)
+                }
+                Priority.MEDIUM -> {
+                    view.setSelection(1)
+                }
+                Priority.LOW -> {
+                    view.setSelection(2)
+                }
+            }
+        }
+
+        @RequiresApi(Build.VERSION_CODES.M)
+        @BindingAdapter("android:parsePriorityColor")
+        @JvmStatic
+        fun parsePriorityColor(cardView: CardView, priority: Priority) {
+            when (priority) {
+                Priority.HIGH -> {
+                    cardView.setCardBackgroundColor(cardView.context.getColor(R.color.red))
+                }
+                Priority.MEDIUM -> {
+                    cardView.setCardBackgroundColor(cardView.context.getColor(R.color.yellow))
+                }
+                Priority.LOW -> {
+                    cardView.setCardBackgroundColor(cardView.context.getColor(R.color.green))
+                }
+            }
+        }
+
+        //navigate to update fragment and send data to that fragment
+        @BindingAdapter("android:sendDataToUpdateFragment")
+        @JvmStatic
+        fun sendDataToUpdateFragment(view: ConstraintLayout, currentItem: ToDoData) {
+            view.setOnClickListener {
+                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentItem)
+                view.findNavController().navigate(action)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/todo/fragments/list/adapter/ListAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.todo.fragments.list.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todo.R
+import com.example.todo.data.models.Priority
+import com.example.todo.data.models.ToDoData
+import com.example.todo.databinding.RowLayoutBinding
+
+//Extend it with RecyclerView Adapter and specifying a viewHolder and creating the class of viewHolder inside it
+class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    //list of toDoData
+    private var datalist = emptyList<ToDoData>()
+
+    //will extend RecyclerView.ViewHolder ,pass view parameters which represents row layout
+    class MyViewHolder(private val binding: RowLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+        //binding function for toDoData object
+        fun bind(toDoData: ToDoData){
+            binding.todoData = toDoData
+            binding.executePendingBindings()
+        }
+
+        //returning view binding
+        companion object{
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RowLayoutBinding.inflate(layoutInflater,parent,false)
+                return MyViewHolder(binding)
+            }
+        }
+    }
+
+    //override 3 methods for ListAdapter(onCreateViewHolder,onBindViewHolder,getItemCount())
+    //get reference of row_layout file using viewBinding
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        //creating currentItem of toDoData and getting dynamically from data list
+        val currentItem = datalist[position]
+        //using holder passing currentItem(toDoData object) to bind function
+        holder.bind(currentItem)
+    }
+
+    //return count of datalist (todoData)
+    override fun getItemCount(): Int {
+        return datalist.size
+    }
+
+    //getting parameters of list todoData
+    fun setData(toDoData:List<ToDoData>){
+        //set toDoData list of parameters to toDoData from ListAdapter
+        this.datalist = toDoData
+        //and notify the changes
+        notifyDataSetChanged()
+    }
+}
